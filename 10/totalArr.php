a3cc3f5b9ef6a95538640df1f8980022324313a2
TenHamamoto <filmbarong10@gmail.com>
2022-07-25 18:57:39 +0900

    fix:10-5 5行目削除

--- 10/totalArr.php
+++ 10/totalArr.php
@@ -2,7 +2,6 @@
 $total = 0;
 $priceList = [298, 348, 198, 680, 98, 980, 498, 640];
 for ($i = 0; $i < count($priceList); $i++) {
-    $priceList[$i];
     $total += $priceList[$i];
 }
 echo $total * 1.1 .'円';
