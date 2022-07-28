b1bfa2a9bafc5a43897f1ea82f3098c7f0a61402
TenHamamoto <filmbarong10@gmail.com>
2022-07-28 11:18:46 +0900

    fix:12-4 入力人数　h関数定義位置　tableタグ

--- 12/season.php
+++ 12/season.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 require_once dirname(__FILE__) . '/Chart.php';
 
@@ -14,6 +16,17 @@ if (!empty($_POST)) {
     $winter = $_POST['winter'];
 }
 
+$data  = [$spring, $summer, $fall, $winter];
+$label = ['春', '夏', '秋', '冬'];
+
+$c = new Chart();
+$c->setTitle('好きな季節 アンケート結果');
+$c->addData($data, 'season');
+$c->setXLabel($label);
+$c->setXAxisName('季節');
+$c->setYAxisName('（人）');
+$c->setSize(300, 200);
+
 /**
  * XSS対策の参照名省略
  *
@@ -25,42 +38,44 @@ function h(?string $string): string
 {
     return htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
 }
-
-$data  = [53, 75, 48, 26];
-$label = ['春','夏','秋','冬'];
-
-$c = new Chart();
-$c->setTitle('好きな季節 アンケート結果');
-$c->addData($data, 'season');
-$c->setXLabel($label);
-$c->setXAxisName('季節');
-$c->setYAxisName('（人）');
-$c->setSize(300, 200);
 ?>
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>集計結果入力</title>
 </head>
+
 <body>
     <h1>集計結果入力</h1>
     <form action="" method="post" novalidate>
-    <table>
-        <tr>
-        <p>春<input type="text" name="spring" size="4" value="<?=h($spring)?>">人</p>
-        <p>夏<input type="text" name="summer" size="4" value="<?=h($summer)?>">人</p>
-        <p>秋<input type="text" name="fall" size="4" value="<?=h($fall)?>">人</p>
-        <p>冬<input type="text" name="winter" size="4" value="<?=h($winter)?>">人</p>
-    </tr>
-    <tr></tr>
-        <p><input type="submit" value="グラフ生成"></p>
+        <table>
+            <tr>
+            <td>春<input type="text" name="spring" size="4" value="<?= h($spring) ?>">人</td>
+            </tr>
+            <tr>
+                <td>夏<input type="text" name="summer" size="4" value="<?= h($summer) ?>">人</td>
+            </tr>
+            <tr>
+                <td>秋<input type="text" name="fall" size="4" value="<?= h($fall) ?>">人</td>
+            </tr>
+            <tr>
+                <td>冬<input type="text" name="winter" size="4" value="<?= h($winter) ?>">人</td>
+            </tr>
+            </tr>
+            <tr>
+            </tr>
+            <tr>
+                <td><input type="submit" value="グラフ生成"></td>
+            </tr>
     </form>
     </table>
-    <?php if ($_SERVER['REQUEST_METHOD'] === 'POST'): ?>
+    <?php if ($_SERVER['REQUEST_METHOD'] === 'POST') : ?>
         <?php $c->printBarChart(); ?>
     <?php endif; ?>
 </body>
+
 </html>
