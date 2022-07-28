daec975988445d8abd7ece05b3d3a21630b847e8
TenHamamoto <filmbarong10@gmail.com>
2022-07-28 14:28:00 +0900

    add:13-1 modify.php 追加仕様

--- 13/modify.php
+++ 13/modify.php
@@ -1,21 +1,33 @@
 <?php
-$d1 = new DateTime('last day of February 2025');
-$d2 = new DateTime();
-$d2->modify('-10 days');
 
-$weekday = ['日', '月', '火', '水', '木', '金', '土'];
-$w = $weekday[$d1->format('w')];
-$w = $weekday[$d2->format('w')];
+/**
+ * 「2025年2月末日」と「現在日時の10日前」
+ *  上記年月日の差と日数を算出する。
+ *
+ * @return string
+ */
+function getJpDate():string
+{
+    $d1 = new DateTime('last day of February 2025');
+    $d2 = new DateTime();
+    $d2->modify('-10 days');
 
-$interval = $d1->diff($d2);
-$days = $interval->days; // 日数の差を取得
-$invert = $interval->invert; // 判定の数値1or0を取得
-if ($days == 0) {
-    echo '日付は同じです';
-} else {
-    if ($invert == 1) {
-        echo $d1->format('Y年m月d日') . '(' . $w . ')' . 'の方が「' . $days .'日分」'. $d2->format('Y年m月d日') . '(' . $w . ')' . 'より新しいです';
+    $weekday = ['日', '月', '火', '水', '木', '金', '土'];
+    $w = $weekday[$d1->format('w')];
+    $w = $weekday[$d2->format('w')];
+
+    $interval = $d1->diff($d2);
+    $days = $interval->days; // 日数の差を取得
+    $invert = $interval->invert; // 判定の数値1or0を取得
+    if ($days == 0) {
+        echo '日付は同じです';
     } else {
-        echo $d2->format('Y年m月d日') . '(' . $w . ')' . 'の方が「' . $days .'日分」'. $d1->format('Y年m月d日') . '(' . $w . ')' . 'より古いです';
+        if ($invert == 1) {
+            $result = $d1->format('Y年m月d日') . '(' . $w . ')' . 'の方が「' . $days . '日分」' . $d2->format('Y年m月d日') . '(' . $w . ')' . 'より新しいです';
+        } else {
+            $result = $d2->format('Y年m月d日') . '(' . $w . ')' . 'の方が「' . $days . '日分」' . $d1->format('Y年m月d日') . '(' . $w . ')' . 'より古いです';
+        }
     }
+    return $result;
 }
+echo getJpDate();
