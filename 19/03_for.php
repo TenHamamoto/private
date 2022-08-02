ec3b003e065f4993e0db685f0ec242dd5845735d
TenHamamoto <filmbarong10@gmail.com>
2022-08-02 10:25:15 +0900

    add:19-2

---
+++ 19/03_for.php
@@ -0,0 +1,28 @@
+<?php
+for ($i = 1; $i <= 9; $i++) {
+    echo $i . ' ';
+}
+for(i=0;i<10;++i){
+    // カウンタ「i」の繰り返し処理
+    for(j=0;j<10;++j){
+      // カウンタ「j」の繰り返し処理
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <table>
+        <tr>
+            
+        </tr>
+    </table>
+</body>
+</html>
