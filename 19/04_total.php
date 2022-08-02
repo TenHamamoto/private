a1587dcc399f6bc0bd3d084f29abd8e48f93225d
TenHamamoto <filmbarong10@gmail.com>
2022-08-02 11:26:55 +0900

    add:19-4

--- 19/04_total.php
+++ 19/04_total.php
@@ -6,4 +6,13 @@ for ($i = 1; $i <= 30; $i++) {
 }
 echo '合計は' . $total . 'です'. '<br>';
 
-$arrNums
+$arrNums = array();
+for ($i = 1; $i <= 100; $i++) {
+    $arrNums[] += $i;
+}
+
+$total2 = 0;
+foreach ($arrNums as $count) {
+    $total2 += $count;
+}
+echo '配列の合計は' . $total2 . 'です';
