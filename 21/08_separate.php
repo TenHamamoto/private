4b0a4043e60760972198af27194b604c36f543f6
TenHamamoto <filmbarong10@gmail.com>
2022-08-09 16:36:46 +0900

    add:21-8 08_separate.php

--- 21/08_separate.php
+++ 21/08_separate.php
@@ -1,39 +1,18 @@
 <?php
 
 $arrNums = [42, 7, 35, 54, 71, 32, 13, 8, 21, 45];
-$even = '偶数：';
-$odd = '奇数：';
-
-// 偶数：42 54 32 8
-// 奇数：7 35 71 13 21 45
-
-echo '<pre>';
-print_r($arrNums);
-echo '</pre>';
-
+$even    = '偶数：';
+$odd     = '奇数：';
 
 for ($i = 0; $i < count($arrNums); $i++) {
 
-    echo $arrNums[$i];
-
-    
-
-
-    echo '<br>';
-}
-
-
-for ($i = 1; $i <= 100; $i++) {
-
-    echo $i;
-
-    if ($i % 3 === 0 && $i % 5 === 0) {
-        echo ':FizzBuzz';
-    } elseif ($i % 3 === 0) {
-        echo ':Fizz';
-    } elseif ($i % 5 === 0) {
-        echo ':Buzz';
+    if(($arrNums[$i] % 2) === 0){
+        $even .= $arrNums[$i] . ' ';
+    }else{
+        $odd .= $arrNums[$i] . ' ';
     }
 
-    echo '<br>';
 }
+echo $even . '<br>' . $odd;
+// 偶数：42 54 32 8
+// 奇数：7 35 71 13 21 45
