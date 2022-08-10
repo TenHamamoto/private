377bec47577d012d58023ca0ed956f2e485d4f9e
TenHamamoto <filmbarong10@gmail.com>
2022-08-10 12:18:18 +0900

    fix:21-12 12_magic.php 1行で記述

--- 21/15_avg.php
+++ 21/15_avg.php
@@ -1,3 +1,27 @@
 <?php
 $arrNums = [1, 7, 11];
 // 平均値は6です。
+
+$prices = [298, 129, 198, 274, 625, 273, 296, 325, 200, 127, 398];
+// 3,457円
+
+/**
+ * 購入商品価格の配列を指定すると、
+ * 10%の税込み価格を返す
+ *
+ * @param array|null $prices
+ * @param integer|null $tax
+ * @return integer
+ */
+function getPriceInTax(?array $prices, ?int $tax = 8): int
+{
+    if (empty($prices)) retrun;
+
+    $total = 0;
+    foreach ($prices as $price) {
+        $total += $price;
+    }
+    return floor($total * (1 + $tax / 100));
+}
+
+echo number_format(getPriceInTax($prices, 10)) . '円';
