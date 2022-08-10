e840223e32ec483b3c936ee2b951926a8fe59fd1
TenHamamoto <filmbarong10@gmail.com>
2022-08-10 14:20:24 +0900

    fix:21-14 14_tax.php PHPDoc,TypeHinting,関数内の計算

--- 21/14_tax.php
+++ 21/14_tax.php
@@ -5,12 +5,12 @@ $price = 9876;
  * 税抜き価格の変数を指定すると、
  * 10%の税込み価格を返す
  *
- * @param integer $price
- * @return integer
+ * @param integer|null $price
+ * @return float
  */
-function getTaxPrice(?int $price): int
+function getTaxPrice(?int $price): float
 {
-    return round($price * 1.1);
+    return floor($price)* 1.1;
 }
 
 echo '税込み価格は' . number_format(getTaxPrice($price)) . '円です。';
