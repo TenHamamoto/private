32413ffcff40a4cecc4246121fb4f6ca2d071e81
TenHamamoto <filmbarong10@gmail.com>
2022-08-10 12:11:37 +0900

    add:21-11 11_bmi.php 追加仕様

--- 21/11_bmi.php
+++ 21/11_bmi.php
@@ -7,4 +7,12 @@ $bmiBMI = number_format(BMI_WEIGHT / (BMI_HEIGHT * 0.01) / (BMI_HEIGHT * 0.01), 
 $bmiProper = number_format((BMI_WEIGHT - (BMI_HEIGHT * 0.01) * (BMI_HEIGHT * 0.01) * 22), 2);
 
 echo '身長は' . BMI_HEIGHT . 'cmで体重は' . BMI_WEIGHT . 'kgのときのBMI値は' . $bmiBMI . 'で、<br>
-適正体重は＋' . $bmiProper . 'kgです。';
+適正体重は+' . $bmiProper . 'kgです。';
+
+//追加仕様
+echo '<br>';
+
+$bmiPlusMinus = $bmiProper > 0 ? '+' : '';
+
+echo '身長は' . BMI_HEIGHT . 'cmで体重は' . BMI_WEIGHT . 'kgのときのBMI値は' . $bmiBMI . 'で、<br>
+適正体重は' . $bmiPlusMinus . $bmiProper . 'kgです。';
